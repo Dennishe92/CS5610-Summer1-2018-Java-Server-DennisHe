6854c08a81a8f0000a80274802c91d1c333c5136
dennishe1230 <he.xu@husky.neu.edu>
2018-05-29 14:51:58 -0400

    added widget model, repository, and service

---
+++ src/main/java/com/example/myapp/services/WidgetService.java
@@ -0,0 +1,39 @@
+package com.example.myapp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.myapp.models.Widget;
+import com.example.myapp.repositories.WidgetRepository;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class WidgetService {
+	@Autowired
+	WidgetRepository repository;
+	
+	@GetMapping("/api/widget")
+	public List<Widget> findAllWidgets() {
+		return (List<Widget>) repository.findAll();
+	}
+	
+	@PostMapping("/api/widget/save")
+	public void saveAllWidgets(@RequestBody List<Widget> widgets) {
+		repository.deleteAll();
+		for(Widget widget: widgets) {
+			repository.save(widget);
+		}
+	}
+	
+	
+	
+	
+
+}
+	
